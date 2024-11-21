5c842fed90a6bb40abf7e30e03630bf696b5a76e
poudelpawan <poudelpawan500@gmail.com>
2024-11-21 16:16:58 +0545

    changed some styles

---
+++ Home.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - STOCKEASE</title>
+    <link rel="stylesheet" href="style.css"> <!-- Link to the CSS file -->
+</head>
+<body>
+    <div class="home-container">
+        <div class="content">
+        <form method="POST" action="login.php.php">
+            <h1>Welcome to STOCKEASE</h1>
+            <p>Manage your stock inventory with ease. Simplify your business with our efficient and user-friendly platform.</p>
+            <a href="login.php" class="home-btn">Get Started</a>
+        </div>
+        <div class="image-section">
+            <!-- Adding the Image -->
+            <img src="image/stockk.jpg" alt="Stock Management" class="stock-image">
+        </div>
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css"> <!-- Link to the CSS file -->
+</head>
+<body>
+    <div class="welcome-text">Welcome to STOCKEASE</div>
+    <form method="POST" action="welcome.php">
+        
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="Name" required>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="12345" required>
+
+        <button type="submit">Login</button>
+        <p class="register-link">
+            Don't have an account? <a href="register.php">Register here</a>.
+            
+        </p>
+    </form>
+</body>
+</html>
